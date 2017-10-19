b8215095e28171fb8348263a91f9bc2865c35073
Eduardo <dudu_dias.lourenco@hotmail.com>
2017-10-19 15:57:10 -0200

    new

---
+++ html/funcoes.js
@@ -0,0 +1,42 @@
+
+	function funcao1() {
+		if(document.formulariovisitantes.nome.value==""){
+    alert("Preencha o campo Nome!");
+	return false;	
+		}
+		if(document.formulariovisitantes.cpf.value==""){
+    alert("Preencha o campo CPF!");
+	return false;	
+		}
+		if(document.formulariovisitantes.rg.value==""){
+    alert("Preencha o campo RG!");
+	return false;	
+		}
+		if(document.formulariovisitantes.endereco.value==""){
+    alert("Preencha o campo Endereço!");
+	return false;	
+		}
+		if(document.formulariovisitantes.bairro.value==""){
+    alert("Preencha o campo Bairro!");
+	return false;	
+		}
+		if(document.formulariovisitantes.numero.value==""){
+    alert("Preencha o campo numero!");
+	return false;	
+		}
+		if(document.formulariovisitantes.apart.value==""){
+    alert("Informe o Apartamento!");
+	return false;		
+		}
+		if(document.formulariovisitantes.celular.value==""){
+    alert("Informe um número de celular!");
+	return false;	
+		}
+		if(document.formulariovisitantes.tel.fixo.value==""){
+    alert("Informe um número de Telefone!");
+	return false;	
+		}
+	}
+	function funcao2() {
+    confirm("Até a próxima!");
+	}
